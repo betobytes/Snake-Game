e8afbb4fe5d42117fcbaf5081c1303869d14fb45
betobytes <bitrox.0001@gmail.com>
2022-10-17 16:33:00 -0500

    ultimate actualization

--- snake.js
+++ snake.js
@@ -1,4 +1,152 @@
-//codigo js de Snake - Game
+let board;
+let context;
+let blockSize = 20;
+let cols = 30;
+let rows = 20;
+
+let appleAudio;
+let gameOverAudio;
+
+let snakeX = 0;
+let snakeY = 0;
+let tail = [];
+
+let foodX = 0;
+let foodY = 0;
+let score = 0;
+
+let velocityX = 1;
+let velocityY = 0;
+    
+let gameOver = false;
+
+window.onload = () => {
+    board = document.getElementById('board');
+    context = board.getContext('2d');
+
+    //appleAudio = new AudioY('Apple_sound.mp3');
+    //gameOverAudio = new Audio('game_over_sound.mp3');
+
+    board.width = cols * blockSize;
+    board.height = rows * blockSize;
+
+    document.addEventListener('keyup', changeDirection);
+    board.addEventListener('click', () => {
+            gameOver = false;
+            score = 0;
+    })
+
+    foodPlace();
+    setInterval(update, 1000/14);//se tienen que poner los frames por second fps 1000/10
+}
+
+function update(){ 
+   //limpiar pantalla;
+   createRect(0, 0, board.width, board.height);
+
+   if(gameOver){
+     //pantalla Game Over
+     createText('Game Over', board.width/2, board.height/2-25, 'center', 50);
+     createText(`Score: ${score}`, board.width/2, board.height/2 + 25, 'center');
+     createText(`Click to restart`, (cols * blockSize) / 2, board.height - 50, 'center');
+
+    return
+   }
+   //imprimir puntaje(score)
+   createText(`Score: ${score}`, 30, 40);
+
+   //crear la comida 
+   createRect (foodX, foodY, blockSize, blockSize, 'red');//se tenian que poner dos dimensiones de blocksize para hacer el cuadro!!
+
+   //comer manzana (interseccion de vectores)
+   if(snakeX == foodX && snakeY == foodY){
+    tail.push([foodX, foodY]);
+    score += 10;
+    //appleAudion.play();
+    foodPlace()
+   }
+
+   //snake tail
+   for(let i = tail.length - 1; i > 0; i--){
+        tail[i] = tail[i - 1];
+   }
+
+   if(tail.length){
+    tail[0] = [snakeX, snakeY];
+   }
+   //snake position 
+   snakeX += velocityX * blockSize;
+   snakeY += velocityY * blockSize;
+    //con creteRecto creamos el cuerpo de la snake    
+   createRect(snakeX, snakeY, blockSize,blockSize, "cyan");
+
+   for(let i = 0; i < tail.length; i++){
+        createRect(tail[i][0], tail[i][1], blockSize, blockSize, "orange");
+   }
+   
+   //chocar con la pared
+   if(snakeX < 0 || snakeX >= cols * blockSize || snakeY < 0 || snakeY >= rows * blockSize){
+        gameOverEvent()
+   }
+   //impact herself
+   for(let i = 0; i < tail.length; i++){
+        if(snakeX == tail[i][0] && snakeY == tail[i][1]){
+            gameOverEvent()
+        }
+   }
+
+}
+
+function foodPlace(){
+    foodX = Math.floor(Math.random() * cols)* blockSize;
+    foodY = Math.floor(Math.random() * rows)* blockSize;
+
+}
+
+function changeDirection(e){
+    if(e.code == "ArrowUp"){
+        velocityX = 0;
+        velocityY = -1;
+    }else if(e.code == "ArrowDown"){
+        velocityX = 0;
+        velocityY = 1;
+    }else if(e.code == "ArrowLeft"){
+        velocityX = -1;
+        velocityY = 0;
+    }else if(e.code == "ArrowRight"){
+        velocityX = 1;
+        velocityY = 0;  
+    }
+}
+
+function gameOverEvent(){
+    gameOver = true;
+    //gameOverAudio.play();
+    tail = [];
+    snakeX = 0;
+    snakeY = 0;
+    velocityX = 1;
+    velocityY = 0;
+
+}
+
+function createRect(x, y, width, height, color = "black"){
+    context.fillStyle = color;
+    context.fillRect(x, y, width, height);
+
+}
+
+function createText(text, x, y, textAlign = "start", fontSize = 20){
+    context.fillStyle = 'cyan';
+    context.font = `${fontSize}px monospace`;
+    context.textAlign = textAlign;
+    context.fillText(text, x, y);
+
+}
+
+
+
+/*//codigo js de Snake - Game con clases
 class Snake{
     constructor(x, y, size){
         this.x = x;
@@ -142,3 +290,4 @@ window.addEventListener("keydown", (event)=>{
         }
     }, 1)
 })
+*/
